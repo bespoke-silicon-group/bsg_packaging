cfa578b8d429f782f1b865cfe21c93027002b8de
Michael Taylor <prof.taylor@gmail.com>
2014-12-10 10:33:33 -0800

    initial file structure

---
+++ common/verilog/bsg_iopad_macros_verilog.v
@@ -0,0 +1,36 @@
+// MBT 9-22-14
+//
+// **********************************************************************
+// BEGIN TOP LEVEL I/O MACROS : BSG ONE (VERILOG; NOT REAL)
+//
+// these macros define the PAD instantiations for general I/Os
+// when we actually don't want to bother instantiating real pads
+//
+// these macros will often be defined on a per-process basis,
+// as the interfaces for the I/O macros change across generations.
+//
+
+
+// PDIDGZ foo_i (.PAD(p_foo_i), .C(foo_i_int));
+`define XTC_IN(name)                    wire name``_i_int = p_``name``_i
+
+`define XTC_IN_A(pre,suffix,letter,index) assign pre``_``suffix``_i_int[index] = p_``pre``_``suffix``_i[index]
+
+`define XTC_IN_V(name,index)            assign name``_i_int[index] = p_``name``_i[index]
+
+
+`define XTC_OUT(name)     wire name``_o_int; \
+                          assign p_``name``_o = name``_o_int
+
+`define XTC_OUT_V(name,index) assign p_``name``_o[index] = name``_o_int[index]
+
+`define XTC_OUT_A(pre,suffix,letter,num) assign p_``pre``_``suffix``_o[num] = pre``_``suffix``_o_int[num]
+
+
+// shared macros
+`include "bsg_iopad_meta_macros.v"
+
+//
+//
+// END TOP LEVEL I/O MACROS (250 nm)
+// **********************************************************************
---
+++ common/verilog/bsg_iopad_meta_macros.v
@@ -0,0 +1,20 @@
+`define XTC_IN_DIFF(zod)  `XTC_IN(zod``_p); `XTC_IN(zod``_n)
+
+`define XTC_OUT_DIFF(name)  `XTC_OUT(name``_p); `XTC_OUT(name``_n)
+
+`define XTC_IN_V70(name)  wire [7:0] name``_i_int; \
+                          `XTC_IN_V(name,0); `XTC_IN_V(name,1); `XTC_IN_V(name,2); `XTC_IN_V(name,3); \
+                          `XTC_IN_V(name,4); `XTC_IN_V(name,5); `XTC_IN_V(name,6); `XTC_IN_V(name,7)
+
+`define XTC_OUT_V70(name) wire [7:0] name``_o_int; \
+                          wire [7:0] name``_oen = '0; \
+                          `XTC_OUT_V(name,0); `XTC_OUT_V(name,1); `XTC_OUT_V(name,2); `XTC_OUT_V(name,3); \
+                          `XTC_OUT_V(name,4); `XTC_OUT_V(name,5); `XTC_OUT_V(name,6); `XTC_OUT_V(name,7)
+
+`define XTC_IN_ABCD(pre,suffix) wire[3:0] pre``_``suffix``_i_int; \
+                                `XTC_IN_A(pre,suffix,A,0); `XTC_IN_A(pre,suffix,B,1); `XTC_IN_A(pre,suffix,C,2); `XTC_IN_A(pre,suffix,D,3)
+
+
+`define XTC_OUT_ABCD(pre,suffix) wire [3:0] pre``_``suffix``_o_int; \
+                                 wire [3:0] pre``_``suffix``_oen = '0; \
+                                 `XTC_OUT_A(pre,suffix,A,0); `XTC_OUT_A(pre,suffix,B,1); `XTC_OUT_A(pre,suffix,C,2); `XTC_OUT_A(pre,suffix,D,3)
