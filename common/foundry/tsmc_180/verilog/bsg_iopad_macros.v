0993053a5492731aa954f4a82f56c32e70f72982
proftaylor <proftaylor@https://bitbucket.org>
2016-08-07 13:14:22 -0700

    add bsg_two pinout

--- common/foundry/tsmc_180/verilog/bsg_iopad_macros.v
+++ common/foundry/tsmc_180/verilog/bsg_iopad_macros.v
@@ -7,39 +7,45 @@
 // as the interfaces for the I/O macros change across generations.
 //
 
-`define INPAD PDIDGZ
 
-// we use 12 mA I/Os as a compromise between electromigration
-// signal integrity, and transition rate
+// this is used to disable a differential input signal
+// even though this is an input
+// we employ an output pad and tri-state (OEN=1) the output
+// we are trying to avoid the case of having an input
+// pad with 1.2 LVDS voltage that creates short-circuit current in a 3.3V LVCMOS pad
+// cell.
 
-`define OUTPAD PDT12DGZ
+`define BSG_IO_IN_DISABLE(name,padtype) \
+padtype name``_disable (.PAD(p_``name``_i), .OEN(1'b1), .I(1'b0))
 
-// PDIDGZ foo_i (.PAD(p_foo_i), .C(foo_i_int));
-`define XTC_IN(name)   wire name``_i_int; \
-                       `INPAD name``_i (.PAD(p_``name``_i), .C(name``_i_int))
+`define BSG_IO_IN_DISABLE_DIFF(name,padtype) \
+  BSG_IO_IN_DISABLE(name``_p,padtype); \
+  BSG_IO_IN_DISABLE(name``_n,padtype);
 
-// PDIDGZ sdi_A_sclk_ex_i (.PAD(p_sdi_sclk_ex_i[0]), .C(sdi_sclk_ex_i_int[0])); --> XTC_IN_AO(sdi,sclk_ex,A,0)
-`define XTC_IN_A(pre,suffix,letter,index) \
-   `INPAD pre``_``letter``_``suffix``_i (.PAD(p_``pre``_``suffix``_i[index]), .C(pre``_``suffix``_i_int[index]))
+// PDDDGZ foo_i (.PAD(p_foo_i), .C(foo_i_int));
+`define BSG_IO_IN(name,padtype)   wire name``_i_int; \
+padtype name``_i (.PAD(p_``name``_i), .C(name``_i_int))
+
+// PDDDGZ sdi_A_sclk_ex_i (.PAD(p_sdi_sclk_ex_i[0]), .C(sdi_sclk_ex_i_int[0])); --> BSG_IO_IN_AO(sdi,sclk_ex,A,0)
+`define BSG_IO_IN_A(pre,suffix,letter,index,padtype) \
+   padtype pre``_``letter``_``suffix``_i (.PAD(p_``pre``_``suffix``_i[index]), .C(pre``_``suffix``_i_int[index]))
 
 
-// PDIDGZ foo_0_i (.PAD(p_foo_i[0]), .C(foo_i_int[0]));
-`define XTC_IN_V(name,index) `INPAD name``_``index``_i (.PAD(p_``name``_i[index]), .C(name``_i_int[index]))
+// PDDDGZ foo_0_i (.PAD(p_foo_i[0]), .C(foo_i_int[0]));
+`define BSG_IO_IN_V(name,index,padtype) padtype name``_``index``_i (.PAD(p_``name``_i[index]), .C(name``_i_int[index]))
 
 
 // PDT12DGZ foo_o (.PAD(p_foo_o), .OEN(foo_oen), .I(foo_o_int))
-`define XTC_OUT(name) wire name``_o_int; \
-                      wire name``_oen = '0; \
-                      `OUTPAD name``_o (.PAD(p_``name``_o), .OEN(name``_oen), .I(name``_o_int))
+`define BSG_IO_OUT(name,padtype) wire name``_o_int; \
+     padtype name``_o (.PAD(p_``name``_o), .I(name``_o_int))
 
 // PDT12DG foo_0_o (.PAD(p_foo_o[0]), .OEN(foo_oen[0]), .I(foo_o_int[0]))
-`define XTC_OUT_V(name,index) `OUTPAD name``_``index``_o (.PAD(p_``name``_o[index]), .OEN(name``_oen[index]), .I(name``_o_int[``index]))
+`define BSG_IO_OUT_V(name,index,padtype) padtype name``_``index``_o (.PAD(p_``name``_o[index]), .I(name``_o_int[``index]))
 
 
-
-// PDT12DGZ sdo_A_sclk_ex_o (.PAD(p_sdo_sclk_ex_o[0]), .OEN(sdo_sclk_ex_oen[0], .I(sdo_sclk_ex_o_int[0]))); --> XTC_OUT_AO(sdo,sclk_ex,A,0)
-`define XTC_OUT_A(pre,suffix,letter,num) \
-  `OUTPAD pre``_``letter``_``suffix``_o (.PAD(p_``pre``_``suffix``_o[num]), .OEN(pre``_``suffix``_oen[num]), .I(pre``_``suffix``_o_int[num]))
+// PDT12DGZ sdo_A_sclk_ex_o (.PAD(p_sdo_sclk_ex_o[0]), .OEN(sdo_sclk_ex_oen[0], .I(sdo_sclk_ex_o_int[0]))); --> BSG_OUT_AO(sdo,sclk_ex,A,0)
+`define BSG_IO_OUT_A(pre,suffix,letter,num,padtype) \
+  padtype pre``_``letter``_``suffix``_o (.PAD(p_``pre``_``suffix``_o[num]), .I(pre``_``suffix``_o_int[num]))
 
 
 // shared macros
--- ucsd_bga_332/pinouts/bsg_two/bsg_pinout.v
+++ ucsd_bga_332/pinouts/bsg_two/bsg_pinout.v
@@ -21,7 +21,7 @@
 // for example bsg_one_iopad_macros_verilog.v
 
 //module
-// bsg_one
+// bsg_two
 (
  // *******************************************************************
  // all "100-ohm impedance-controlled-in-package" differential pairs
@@ -111,60 +111,111 @@
  , input  p_PLL_VZZ_i
  );
 
-   // note this is a usually the primary clock for the system
-   `XTC_IN (PLL_CLK);
-   `XTC_IN (reset);
+`ifndef BSG_PAD_INPUT_PULLDOWN
+BSG_PAD_INPUT_PULLDOWN should be defined!
+`endif
 
-   `XTC_IN_ABCD (sdi,sclk);
-   `XTC_IN_ABCD (sdi,sclk_ex);
-   `XTC_IN_ABCD (sdi,ncmd);
-   `XTC_OUT_ABCD(sdi,token);
-   `XTC_OUT_ABCD(sdi,tkn_ex);
-   `XTC_IN_V70  (sdi_A_data);  `XTC_IN_V70 (sdi_B_data); `XTC_IN_V70 (sdi_C_data); `XTC_IN_V70 (sdi_D_data);
+`ifndef BSG_PAD_OUTPUT_A
+BSG_PAD_OUTPUT_A should be defined!
+`endif
+
+`ifndef BSG_PAD_OUTPUT_B
+BSG_PAD_OUTPUT_B should be defined!
+`endif
+
+`ifndef BSG_PAD_OUTPUT_C
+BSG_PAD_OUTPUT_C should be defined!
+`endif
+
+`ifndef BSG_PAD_OUTPUT_D
+BSG_PAD_OUTPUT_D should be defined!
+`endif
+
+`ifndef BSG_PAD_OUTPUT_SLOW
+BSG_PAD_OUTPUT_SLOW should be defined!
+`endif
+
+`ifndef BSG_PAD_DISABLE
+BSG_PAD_DISABLE should be defined!
+`endif
+
+   // note this is a usually the primary external clock for the system
+   `BSG_IO_IN (PLL_CLK,`BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN (reset,  `BSG_PAD_INPUT_PULLDOWN);
+
+   `BSG_IO_IN_ABCD (sdi,sclk,    `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_ABCD (sdi,sclk_ex, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_ABCD (sdi,ncmd,    `BSG_PAD_INPUT_PULLDOWN);
+
+   `BSG_IO_OUT_ABCD(sdi,token,   `BSG_PAD_OUTPUT_A,    `BSG_PAD_OUTPUT_B,    `BSG_PAD_OUTPUT_C,    `BSG_PAD_OUTPUT_D   );
+   `BSG_IO_OUT_ABCD(sdi,tkn_ex,  `BSG_PAD_OUTPUT_SLOW, `BSG_PAD_OUTPUT_SLOW, `BSG_PAD_OUTPUT_SLOW, `BSG_PAD_OUTPUT_SLOW);
+
+   `BSG_IO_IN_V70 (sdi_A_data, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_V70 (sdi_B_data, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_V70 (sdi_C_data, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_V70 (sdi_D_data, `BSG_PAD_INPUT_PULLDOWN);
 
 
-   `XTC_OUT_ABCD(sdo,sclk);
-   `XTC_OUT_ABCD(sdo,sclk_ex);
-   `XTC_OUT_ABCD(sdo,ncmd);
-   `XTC_IN_ABCD (sdo,token);
-   `XTC_IN_ABCD (sdo,tkn_ex);
-   `XTC_OUT_V70 (sdo_A_data);  `XTC_OUT_V70(sdo_B_data); `XTC_OUT_V70(sdo_C_data); `XTC_OUT_V70(sdo_D_data);
+   `BSG_IO_OUT_ABCD(sdo,sclk,     `BSG_PAD_OUTPUT_A, `BSG_PAD_OUTPUT_B, `BSG_PAD_OUTPUT_C, `BSG_PAD_OUTPUT_D);
+
+   // we make these fast because they are used as data for 10-bit channels
+   `BSG_IO_OUT_ABCD(sdo,sclk_ex,  `BSG_PAD_OUTPUT_A, `BSG_PAD_OUTPUT_B, `BSG_PAD_OUTPUT_C, `BSG_PAD_OUTPUT_D);
+   `BSG_IO_OUT_ABCD(sdo,ncmd,     `BSG_PAD_OUTPUT_A, `BSG_PAD_OUTPUT_B, `BSG_PAD_OUTPUT_C, `BSG_PAD_OUTPUT_D);
+
+   `BSG_IO_IN_ABCD (sdo,token,  `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_ABCD (sdo,tkn_ex, `BSG_PAD_INPUT_PULLDOWN);
+
+   `BSG_IO_OUT_V70 (sdo_A_data, `BSG_PAD_OUTPUT_A);
+   `BSG_IO_OUT_V70 (sdo_B_data, `BSG_PAD_OUTPUT_B);
+   `BSG_IO_OUT_V70 (sdo_C_data, `BSG_PAD_OUTPUT_C);
+   `BSG_IO_OUT_V70 (sdo_D_data, `BSG_PAD_OUTPUT_D);
 
     // node L3 and R3 have been chosen as outputs
 
-   `XTC_IN_V60 (misc_L7654210);
-   `XTC_IN_V60 (misc_R7654210);
+   `BSG_IO_IN_V60 (misc_L7654210,  `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN_V60 (misc_R7654210,  `BSG_PAD_INPUT_PULLDOWN);
 
    // note these are not part of the vector as bit number is in name.
-   `XTC_IN (misc_T_0);
-   `XTC_IN (misc_T_1);
-   `XTC_IN (misc_T_2);
+   `BSG_IO_IN (misc_T_0,`BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN (misc_T_1,`BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN (misc_T_2,`BSG_PAD_INPUT_PULLDOWN);
 
-   `XTC_OUT(sdo_A_data_8);
-   `XTC_OUT(sdo_C_data_8);
-   `XTC_OUT(misc_L_3);
-   `XTC_OUT(misc_R_3);
+    // these can be used as fast i/os
+   `BSG_IO_OUT(sdo_A_data_8,`BSG_PAD_OUTPUT_A);
+   `BSG_IO_OUT(sdo_C_data_8,`BSG_PAD_OUTPUT_C);
+
+   `BSG_IO_OUT(misc_L_3,`BSG_PAD_OUTPUT_SLOW);
+   `BSG_IO_OUT(misc_R_3,`BSG_PAD_OUTPUT_SLOW);
 
    // differential pairs
-   `XTC_IN_DIFF(clk_0);
-   `XTC_IN_DIFF(clk_1);
-   `XTC_IN_DIFF(SMA_in);
-   `XTC_OUT_DIFF(SMA_out);
+   `BSG_IO_IN_DIFF_DISABLE (clk_0, `BSG_PAD_DISABLE)
+   `BSG_IO_IN_DIFF_DISABLE (clk_1, `BSG_PAD_DISABLE)
+   `BSG_IO_IN_DIFF_DISABLE (SMA_in,`BSG_PAD_PISABLE)
 
-   `XTC_IN(JTAG_TMS);
-   `XTC_IN(JTAG_TDI);
-   `XTC_IN(JTAG_TCK);
-   `XTC_IN(JTAG_TRST);
-   `XTC_OUT(JTAG_TDO);
+    // these are routed as 100 ohms to SMA connector
+    // they do not have a differential termination resistor
+    // on-board, so they can be driven with normal outputs
+    // and used for debug.
+
+   `BSG_IO_OUT(SMA_out_p,`BSG_PAD_OUTPUT_SLOW)
+   `BSG_IO_OUT(SMA_out_n,`BSG_PAD_OUTPUT_SLOW)
+
+   `BSG_IO_IN(JTAG_TMS, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN(JTAG_TDI, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN(JTAG_TCK, `BSG_PAD_INPUT_PULLDOWN);
+   `BSG_IO_IN(JTAG_TRST,`BSG_PAD_INPUT_PULLDOWN);
+
+   // debug output
+   `BSG_IO_OUT(JTAG_TDO,`BSG_PAD_OUTPUT_SLOW);
 
    // these are dedicated power pins and should not be
    // instantiated as pins in the verilog
    //
-   //`XTC_IN(PLL_VDD);
-   //`XTC_IN(PLL_VSS);
+   //`BSG_IO_IN(PLL_VDD);
+   //`BSG_IO_IN(PLL_VSS);
 
-   //`XTC_IN(PLL_V33);
-   //`XTC_IN(PLL_VZZ);
+   //`BSG_IO_IN(PLL_V33);
+   //`BSG_IO_IN(PLL_VZZ);
 
 // End UCSD BGA 332 PAD Definitions
 // **********************************************************************
