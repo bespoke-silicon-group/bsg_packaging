e055426605ee24ee0a3714056a0832f2df578027
proftaylor <proftaylor@https://bitbucket.org>
2016-08-07 09:21:08 -0700

    prepare for 180

---
+++ common/foundry/tsmc_180/verilog/bsg_iopad_macros.v
@@ -0,0 +1,51 @@
+
+// **********************************************************************
+// BEGIN TOP LEVEL I/O MACROS : BSG TWO (TSMC 180 nm)
+//
+// these macros define the PAD instantiations for general I/Os
+// these macros will often be defined on a per-process basis,
+// as the interfaces for the I/O macros change across generations.
+//
+
+`define INPAD PDIDGZ
+
+// we use 12 mA I/Os as a compromise between electromigration
+// signal integrity, and transition rate
+
+`define OUTPAD PDT12DGZ
+
+// PDIDGZ foo_i (.PAD(p_foo_i), .C(foo_i_int));
+`define XTC_IN(name)   wire name``_i_int; \
+                       `INPAD name``_i (.PAD(p_``name``_i), .C(name``_i_int))
+
+// PDIDGZ sdi_A_sclk_ex_i (.PAD(p_sdi_sclk_ex_i[0]), .C(sdi_sclk_ex_i_int[0])); --> XTC_IN_AO(sdi,sclk_ex,A,0)
+`define XTC_IN_A(pre,suffix,letter,index) \
+   `INPAD pre``_``letter``_``suffix``_i (.PAD(p_``pre``_``suffix``_i[index]), .C(pre``_``suffix``_i_int[index]))
+
+
+// PDIDGZ foo_0_i (.PAD(p_foo_i[0]), .C(foo_i_int[0]));
+`define XTC_IN_V(name,index) `INPAD name``_``index``_i (.PAD(p_``name``_i[index]), .C(name``_i_int[index]))
+
+
+// PDT12DGZ foo_o (.PAD(p_foo_o), .OEN(foo_oen), .I(foo_o_int))
+`define XTC_OUT(name) wire name``_o_int; \
+                      wire name``_oen = '0; \
+                      `OUTPAD name``_o (.PAD(p_``name``_o), .OEN(name``_oen), .I(name``_o_int))
+
+// PDT12DG foo_0_o (.PAD(p_foo_o[0]), .OEN(foo_oen[0]), .I(foo_o_int[0]))
+`define XTC_OUT_V(name,index) `OUTPAD name``_``index``_o (.PAD(p_``name``_o[index]), .OEN(name``_oen[index]), .I(name``_o_int[``index]))
+
+
+
+// PDT12DGZ sdo_A_sclk_ex_o (.PAD(p_sdo_sclk_ex_o[0]), .OEN(sdo_sclk_ex_oen[0], .I(sdo_sclk_ex_o_int[0]))); --> XTC_OUT_AO(sdo,sclk_ex,A,0)
+`define XTC_OUT_A(pre,suffix,letter,num) \
+  `OUTPAD pre``_``letter``_``suffix``_o (.PAD(p_``pre``_``suffix``_o[num]), .OEN(pre``_``suffix``_oen[num]), .I(pre``_``suffix``_o_int[num]))
+
+
+// shared macros
+`include "bsg_iopad_meta_macros.v"
+
+//
+//
+// END TOP LEVEL I/O MACROS (180 nm)
+// **********************************************************************
---
+++ ucsd_bga_332/pinouts/bsg_two/bsg_pinout.v
@@ -0,0 +1,172 @@
+// MBT 8-17-2016
+//
+// **********************************************************************
+// UCSD BSG Two Pinout (Based on UCSD BGA 332)
+//
+// This file defines all of the pads that are used for a given pinout of
+// a package. It would change if we reversed the direction of a pad. It
+// is recommended not to change the name of the
+// pin (except the _i or _o) at this level so that we can maintain canonical
+// naming for the package; instead just do an assign of a wire to rename.
+//
+// convention:
+//
+// - the input ports have a prefix of "p_"
+// - the corresponding signals have no prefix
+//   and have a suffix of _int.
+// - the IO pad names are consistent with the
+//   BGA 332 pinout names (e.g, sdi_B_data_7_i, sdi_B_ncmd_i)
+//
+// it is customary to include a macro file ahead of this one;
+// for example bsg_one_iopad_macros_verilog.v
+
+//module
+// bsg_one
+(
+ // *******************************************************************
+ // all "100-ohm impedance-controlled-in-package" differential pairs
+ //
+
+ , input  p_clk_0_p_i    , input  p_clk_0_n_i
+ , input  p_clk_1_p_i    , input  p_clk_1_n_i
+
+ , input  p_SMA_in_p_i   , input  p_SMA_in_n_i
+ , output p_SMA_out_p_o  , output p_SMA_out_n_o
+
+ // *******************************************************************
+ // ultra-shielded "50-ohm impedance-controlled-in-package" clock for PLL
+ , input   p_PLL_CLK_i
+
+ // *******************************************************************
+ // all "50-ohm impedance-controlled-in-package" signals starting here
+ // these are length matched within 1.55 mm length and
+ // .76 mm bond wire length.
+
+ // input clocks for input channels D,C,B,A
+ , input  [3:0] p_sdi_sclk_i
+
+ // input valids for input channels D,C,B,A
+ , input  [3:0] p_sdi_ncmd_i
+
+ // input datas for input channels D,C,B,A
+ , input  [7:0] p_sdi_A_data_i, input [7:0] p_sdi_B_data_i
+ , input  [7:0] p_sdi_C_data_i, input [7:0] p_sdi_D_data_i
+
+ // output tokens for input channels D,C,B,A
+ , output [3:0] p_sdi_token_o
+
+ // output clocks for output channels D,C,B,A
+ , output [3:0] p_sdo_sclk_o
+
+ // output valids for output channels
+ , output [3:0] p_sdo_ncmd_o
+
+ // output data for out channels
+ , output [7:0] p_sdo_A_data_o, output [7:0] p_sdo_B_data_o
+ , output [7:0] p_sdo_C_data_o, output [7:0] p_sdo_D_data_o
+
+ // extra output datas
+ , output p_sdo_A_data_8_o , output p_sdo_C_data_8_o
+
+ // input tokens for output channels
+ , input  [3:0] p_sdo_token_i
+
+ // spare clocks, length matched to channels
+ , input  [3:0] p_sdi_sclk_ex_i
+ , output [3:0] p_sdo_sclk_ex_o
+
+ // spare tokens for input and output channels
+ , output [3:0] p_sdi_tkn_ex_o
+ , input  [3:0] p_sdo_tkn_ex_i
+
+ // *******************************************************************
+ // all "50-ohm impedance-controlled-in-package", starting here
+ // but are next to clock or token signals
+ // and are not length-matched
+
+ , input  p_misc_T_0_i, input p_misc_T_1_i, input p_misc_T_2_i
+
+ // L3 and R3 are output pads because it works with the pad ring
+ , output p_misc_L_3_o, output p_misc_R_3_o
+
+ , input [6:0] p_misc_L7654210_i, input [7:0] p_misc_R7654210_i
+
+ , input  p_reset_i
+
+ // for JTAG, or other purposes
+ , input  p_JTAG_TMS_i
+ , input  p_JTAG_TDI_i
+ , input  p_JTAG_TCK_i
+ , input  p_JTAG_TRST_i
+ , output p_JTAG_TDO_o
+
+ // *******************************************************************
+ // all "not impedance controlled in package", starting here
+ // for powering PLL or can be used for low frequency debug signals
+ // NOTE: Driver selected by XTC macro must be updated according to use.
+
+ , input  p_PLL_VDD_i
+ , input  p_PLL_VSS_i
+ , input  p_PLL_V33_i
+ , input  p_PLL_VZZ_i
+ );
+
+   // note this is a usually the primary clock for the system
+   `XTC_IN (PLL_CLK);
+   `XTC_IN (reset);
+
+   `XTC_IN_ABCD (sdi,sclk);
+   `XTC_IN_ABCD (sdi,sclk_ex);
+   `XTC_IN_ABCD (sdi,ncmd);
+   `XTC_OUT_ABCD(sdi,token);
+   `XTC_OUT_ABCD(sdi,tkn_ex);
+   `XTC_IN_V70  (sdi_A_data);  `XTC_IN_V70 (sdi_B_data); `XTC_IN_V70 (sdi_C_data); `XTC_IN_V70 (sdi_D_data);
+
+
+   `XTC_OUT_ABCD(sdo,sclk);
+   `XTC_OUT_ABCD(sdo,sclk_ex);
+   `XTC_OUT_ABCD(sdo,ncmd);
+   `XTC_IN_ABCD (sdo,token);
+   `XTC_IN_ABCD (sdo,tkn_ex);
+   `XTC_OUT_V70 (sdo_A_data);  `XTC_OUT_V70(sdo_B_data); `XTC_OUT_V70(sdo_C_data); `XTC_OUT_V70(sdo_D_data);
+
+    // node L3 and R3 have been chosen as outputs
+
+   `XTC_IN_V60 (misc_L7654210);
+   `XTC_IN_V60 (misc_R7654210);
+
+   // note these are not part of the vector as bit number is in name.
+   `XTC_IN (misc_T_0);
+   `XTC_IN (misc_T_1);
+   `XTC_IN (misc_T_2);
+
+   `XTC_OUT(sdo_A_data_8);
+   `XTC_OUT(sdo_C_data_8);
+   `XTC_OUT(misc_L_3);
+   `XTC_OUT(misc_R_3);
+
+   // differential pairs
+   `XTC_IN_DIFF(clk_0);
+   `XTC_IN_DIFF(clk_1);
+   `XTC_IN_DIFF(SMA_in);
+   `XTC_OUT_DIFF(SMA_out);
+
+   `XTC_IN(JTAG_TMS);
+   `XTC_IN(JTAG_TDI);
+   `XTC_IN(JTAG_TCK);
+   `XTC_IN(JTAG_TRST);
+   `XTC_OUT(JTAG_TDO);
+
+   // these are dedicated power pins and should not be
+   // instantiated as pins in the verilog
+   //
+   //`XTC_IN(PLL_VDD);
+   //`XTC_IN(PLL_VSS);
+
+   //`XTC_IN(PLL_V33);
+   //`XTC_IN(PLL_VZZ);
+
+// End UCSD BGA 332 PAD Definitions
+// **********************************************************************
+
+
