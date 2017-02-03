e5a6c8420e9997099e2f0312a7c60d0185abc9d5
Scott Davidson <scottdavidson1992@gmail.com>
2017-02-03 17:17:13 -0500

    adding tsmc_16 files

---
+++ ucsd_bga_332/pinouts/bsg_two/tsmc_16/verilog/padmappings/one/bsg_padmapping.v
@@ -0,0 +1,9 @@
+// pull in the macros that define I/Os for the chip
+// this can also be swapped out for other technology files
+// like verilog.
+
+`define BSG_OUTPUTS_NEED_ENABLES
+
+`define BSG_PAD_BIDIR_H PBIDIRN_18_18_FS_DR_H
+`define BSG_PAD_BIDIR_V PBIDIRN_18_18_FS_DR_V
+
