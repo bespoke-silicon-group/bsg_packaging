fbd4a5264ebce46ec6e9dac6df7458b2e83c701a
Scott Davidson <scottdavidson1992@gmail.com>
2017-02-24 15:08:40 -0500

    updating io pad macros for tsmc_16 foundry

--- common/foundry/tsmc_16/verilog/bsg_iopad_macros.v
+++ common/foundry/tsmc_16/verilog/bsg_iopad_macros.v
@@ -1,142 +1,284 @@
 
-// **********************************************************************
-// BEGIN TOP LEVEL I/O MACROS : BSG TWO (TSMC 180 nm)
+// *****************************************************************************
+// BEGIN TOP LEVEL I/O MACROS : BSG TWO (TSMC 16 nm)
 //
 // these macros define the PAD instantiations for general I/Os
 // these macros will often be defined on a per-process basis,
 // as the interfaces for the I/O macros change across generations.
 //
 
+// BIDIR Pad Port Definitions
+//  OE  -- output enable (active high)
+//  A   -- data from core to pad
+//  IE  -- input enable (active high)
+//  IS  -- input select (0: cmos, 1: schmitt)
+//  Y   -- data from pad to core
+//  PE  -- weak pull enable (active high)
+//  PS  -- pull select (0: pull down, 1: pull high)
+//  PAD -- external pad...
+//  SNS -- sense input pin (0: output tri-state, 1: output normal)
+//  SR  -- slew rate (0: fast, 1: slow)
+//  DS0 -- output drive select
+//  DS1 -- output drive select
+//                   DS0 | DS1 | Ouput Drive
+//                  -----+-----+-------------
+//                    0  |  0  |     2mA
+//                    0  |  1  |     4mA
+//                    1  |  0  |     8mA
+//                    1  |  1  |    12mA
+//  PO  -- parametric inverted data (pad to core)
+//  POE -- parametric output enable
 
-`ifdef BSG_OUTPUTS_NEED_ENABLES
-`define BSG_OUTPUT_ENABLE(name)  ,.OE(name)
-`define BSG_OUTPUT_ENABLE_WIRE(name) wire name;
-`else
-error BSG_OUTPUT_NEEDS_ENABLES not defined
-`define BSG_OUTPUT_ENABLE(name)
-`define BSG_OUTPUT_ENABLE_WIRE(name)
-`endif
-
-
-// **************************************************************************************************************
+// *****************************************************************************
+//
+//  OUTPUT PAD PARAMETERIZATION
 //
 
-`define BSG_IO_TIEHI(name)                           \
-  bsg_tiehi #(.width_p(1)) name``tiehi (.o(name));
+// Set ouput drive strength to 2mA
+//`define OUTPUT_DRIVE_SELECT_0   1'b0
+//`define OUTPUT_DRIVE_SELECT_1   1'b0
 
-`define BSG_IO_TIELO(name)                           \
-  bsg_tielo #(.width_p(1)) name``tielo (.o(name));
+// Set ouput drive strength to 4mA
+//`define OUTPUT_DRIVE_SELECT_0   1'b0
+//`define OUTPUT_DRIVE_SELECT_1   1'b1
 
-`define BSG_IO_TIELO_VEC_ONE(name,item)                           \
-  bsg_tielo #(.width_p(1)) name``_``_item``tielo (.o(name[item]));
+// Set ouput drive strength to 8mA
+//`define OUTPUT_DRIVE_SELECT_0   1'b1
+//`define OUTPUT_DRIVE_SELECT_1   1'b0
 
-`define BSG_IO_TIEHI_VEC_ONE(name,item)                           \
-  bsg_tiehi #(.width_p(1)) name``_``_item``tiehi (.o(name[item]));
+// Set ouput drive strength to 12mA
+`define OUTPUT_DRIVE_SELECT_0   1'b1
+`define OUTPUT_DRIVE_SELECT_1   1'b1
 
-`define BSG_IO_TIEHI_VEC(name,width)             \
-                                                 \
-for (i=0; i < width;i=i+1)                       \
-begin: tiehi_``name                              \
-   bsg_tiehi #(.width_p(1)) hi (.o(name[i]));    \
-end
+// Set slew rate (0 = fast, 1 = slow)
+`define OUTPUT_SLEW_RATE   1'b0
 
-`define BSG_IO_TIELO_VEC(name,width)             \
-                                                 \
-for (i=0; i < width;i=i+1)                       \
-begin: tielo_``name                              \
-   bsg_tielo #(.width_p(1)) lo (.o(name[i]));    \
-end
+`define BIDIR_PAD_OUTPUT_ENABLED(pad,a) \
+    .OE(1'b1),                    \
+    .A(a),                        \
+    .IE(1'b0),                    \
+    .IS(1'b0),                    \
+    .Y(),                         \
+    .PE(1'b0),                    \
+    .PS(1'b0),                    \
+    .PAD(pad),                    \
+    .SNS(1'b1),                   \
+    .SR(`OUTPUT_SLEW_RATE),       \
+    .DS0(`OUTPUT_DRIVE_SELECT_0), \
+    .DS1(`OUTPUT_DRIVE_SELECT_1), \
+    .PO(),                        \
+    .POE(1'b0),                   \
+    .RTO(1'b1)
 
+`define BIDIR_PAD_OUTPUT_DISABLE_HI_Z(pad,a) \
+    .OE(1'b0),                    \
+    .A(a),                        \
+    .IE(1'b0),                    \
+    .IS(1'b0),                    \
+    .Y(),                         \
+    .PE(1'b0),                    \
+    .PS(1'b0),                    \
+    .PAD(pad),                    \
+    .SNS(1'b1),                   \
+    .SR(`OUTPUT_SLEW_RATE),       \
+    .DS0(`OUTPUT_DRIVE_SELECT_0), \
+    .DS1(`OUTPUT_DRIVE_SELECT_1), \
+    .PO(),                        \
+    .POE(1'b0),                   \
+    .RTO(1'b1)
 
-`define BSG_IO_IN_DISABLE(name,padtype)                                       \
-                                                                              \
-  wire name``_oen_thi;                                                        \
-  wire name``_tlo;                                                            \
-                                                                              \
-  `BSG_IO_TIEHI(name``_oen_thi)                                               \
-  `BSG_IO_TIELO(name``_tlo)                                                   \
-                                                                              \
-                                                                              \
-  padtype name``_i (.PAD(p_``name``_i), .OE(name``_oen_thi), .A(name``_tlo));
+`define BIDIR_PAD_OUTPUT_DISABLE_PULL_DOWN(pad,a) \
+    .OE(1'b0),                    \
+    .A(a),                        \
+    .IE(1'b0),                    \
+    .IS(1'b0),                    \
+    .Y(),                         \
+    .PE(1'b1),                    \
+    .PS(1'b0),                    \
+    .PAD(pad),                    \
+    .SNS(1'b1),                   \
+    .SR(`OUTPUT_SLEW_RATE),       \
+    .DS0(`OUTPUT_DRIVE_SELECT_0), \
+    .DS1(`OUTPUT_DRIVE_SELECT_1), \
+    .PO(),                        \
+    .POE(1'b0),                   \
+    .RTO(1'b1)
 
-// this is used to disable a differential input signal
-// even though this is an input
-// we employ an output pad and tri-state (OEN=1) the output
-// we are trying to avoid the case of having an input
-// pad with 1.2V LVDS voltage that creates short-circuit current in a 3.3V LVCMOS pad
-// cell.
+`define BIDIR_PAD_OUTPUT_DISABLE_PULL_UP(pad,a) \
+    .OE(1'b0),                    \
+    .A(a),                        \
+    .IE(1'b0),                    \
+    .IS(1'b0),                    \
+    .Y(),                         \
+    .PE(1'b1),                    \
+    .PS(1'b1),                    \
+    .PAD(pad),                    \
+    .SNS(1'b1),                   \
+    .SR(`OUTPUT_SLEW_RATE),       \
+    .DS0(`OUTPUT_DRIVE_SELECT_0), \
+    .DS1(`OUTPUT_DRIVE_SELECT_1), \
+    .PO(),                        \
+    .POE(1'b0),                   \
+    .RTO(1'b1)
 
-`define BSG_IO_IN_DIFF_DISABLE(name,padtype)  \
-                                              \
-  `BSG_IO_IN_DISABLE(name``_p,padtype)        \
-  `BSG_IO_IN_DISABLE(name``_n,padtype)
+// *****************************************************************************
+//
+//  INPUT PAD PARAMETERIZATION
+//
 
+// Set the input trigger type (0: normal cmos, 1: schmitt trigger)
+`define INPUT_TRIGGER_SELECT   1'b0
 
-// **************************************************************************************************************
-// PDDDGZ foo_i (.PAD(p_foo_i), .C(foo_i_int));
+`define BIDIR_PAD_INPUT_ENABLE(pad,y) \
+    .OE(1'b0),                  \
+    .A(1'b0),                   \
+    .IE(1'b1),                  \
+    .IS(`INPUT_TRIGGER_SELECT), \
+    .Y(y),                      \
+    .PE(1'b0),                  \
+    .PS(1'b0),                  \
+    .PAD(pad),                  \
+    .SNS(1'b1),                 \
+    .SR(1'b0),                  \
+    .DS0(1'b0),                 \
+    .DS1(1'b0),                 \
+    .PO(),                      \
+    .POE(1'b0),                 \
+    .RTO(1'b1)
 
-`define BSG_IO_IN(name,padtype)                            \
-                                                           \
-   wire name``_i_int;                                      \
-                                                           \
-   padtype name``_i (.PAD(p_``name``_i), .Y(name``_i_int));
+`define BIDIR_PAD_INPUT_ENABLE_PULL_DOWN(pad,y) \
+    .OE(1'b0),                  \
+    .A(1'b0),                   \
+    .IE(1'b1),                  \
+    .IS(`INPUT_TRIGGER_SELECT), \
+    .Y(y),                      \
+    .PE(1'b1),                  \
+    .PS(1'b0),                  \
+    .PAD(pad),                  \
+    .SNS(1'b1),                 \
+    .SR(1'b0),                  \
+    .DS0(1'b0),                 \
+    .DS1(1'b0),                 \
+    .PO(),                      \
+    .POE(1'b0),                 \
+    .RTO(1'b1)
 
+`define BIDIR_PAD_INPUT_ENABLE_PULL_UP(pad,y) \
+    .OE(1'b0),                  \
+    .A(1'b0),                   \
+    .IE(1'b1),                  \
+    .IS(`INPUT_TRIGGER_SELECT), \
+    .Y(y),                      \
+    .PE(1'b1),                  \
+    .PS(1'b1),                  \
+    .PAD(pad),                  \
+    .SNS(1'b1),                 \
+    .SR(1'b0),                  \
+    .DS0(1'b0),                 \
+    .DS1(1'b0),                 \
+    .PO(),                      \
+    .POE(1'b0),                 \
+    .RTO(1'b1)
 
-// **************************************************************************************************************
-// PDDDGZ sdi_A_sclk_ex_i (.PAD(p_sdi_sclk_ex_i[0]), .C(sdi_sclk_ex_i_int[0])); --> BSG_IO_IN_AO(sdi,sclk_ex,A,0)
+`define BIDIR_PAD_INPUT_DISABLE(pad,y) \
+    .OE(1'b0),                  \
+    .A(1'b0),                   \
+    .IE(1'b1),                  \
+    .IS(`INPUT_TRIGGER_SELECT), \
+    .Y(y),                      \
+    .PE(1'b0),                  \
+    .PS(1'b0),                  \
+    .PAD(pad),                  \
+    .SNS(1'b1),                 \
+    .SR(1'b0),                  \
+    .DS0(1'b0),                 \
+    .DS1(1'b0),                 \
+    .PO(),                      \
+    .POE(1'b0),                 \
+    .RTO(1'b1)
 
-`define BSG_IO_IN_A(pre,suffix,letter,index,padtype)                                                                \
-                                                                                                                    \
-   padtype pre``_``letter``_``suffix``_i (.PAD(p_``pre``_``suffix``_i[index]), .Y(pre``_``suffix``_i_int[index]));
+// *****************************************************************************
+//
+// Begin Tie Cell Definitions
+//
 
+`define BSG_IO_TIEHI(name) \
+    bsg_tiehi #(.width_p(1)) name``tiehi (.o(name));
 
+`define BSG_IO_TIELO(name) \
+    bsg_tielo #(.width_p(1)) name``tielo (.o(name));
 
-// **************************************************************************************************************
-// PDDDGZ foo_0_i (.PAD(p_foo_i[0]), .C(foo_i_int[0]));
+`define BSG_IO_TIELO_VEC_ONE(name,item) \
+    bsg_tielo #(.width_p(1)) name``_``_item``tielo (.o(name[item]));
 
-`define BSG_IO_IN_V(name,index,padtype) padtype name``_``index``_i (.PAD(p_``name``_i[index]), .Y(name``_i_int[index]));
+`define BSG_IO_TIEHI_VEC_ONE(name,item) \
+    bsg_tiehi #(.width_p(1)) name``_``_item``tiehi (.o(name[item]));
 
+`define BSG_IO_TIEHI_VEC(name,width)               \
+    for (i=0; i < width; i=i+1)                    \
+      begin: tiehi_``name                          \
+        bsg_tiehi #(.width_p(1)) hi (.o(name[i])); \
+      end
+`define BSG_IO_TIELO_VEC(name,width)               \
+    for (i=0; i < width; i=i+1)                    \
+      begin: tielo_``name                          \
+        bsg_tielo #(.width_p(1)) lo (.o(name[i])); \
+      end
 
+// *****************************************************************************
+//
+// Begin IO Input Definitions
+//
 
-// **************************************************************************************************************
-// PDT12DGZ foo_o (.PAD(p_foo_o), .OE(foo_oen_int), .I(foo_o_int))
+`define BSG_IO_IN_DISABLE(name,padtype) \
+    wire name``_tlo;                    \
+    `BSG_IO_TIELO(name``_tlo)           \
+    padtype name``_i (`BIDIR_PAD_INPUT_DISABLE(p_``name``_i, name``_tlo));
 
-`define BSG_IO_OUT(name,padtype)                                                                \
-                                                                                                \
-   wire name``_o_int;                                                                           \
-                                                                                                \
-   `BSG_OUTPUT_ENABLE_WIRE(name``_oen_int)                                                       \
-                                                                                                \
-   padtype name``_o (.PAD(p_``name``_o) `BSG_OUTPUT_ENABLE(name``_oen_int) , .A(name``_o_int));
+`define BSG_IO_IN_DIFF_DISABLE(name,padtype) \
+    `BSG_IO_IN_DISABLE(name``_p,padtype)     \
+    `BSG_IO_IN_DISABLE(name``_n,padtype)
 
+`define BSG_IO_IN(name,padtype) \
+    wire name``_i_int;          \
+    padtype name``_i (`BIDIR_PAD_INPUT_ENABLE(p_``name``_i, name``_i_int));
 
+`define BSG_IO_IN_A(pre,suffix,letter,index,padtype) \
+    padtype pre``_``letter``_``suffix``_i (`BIDIR_PAD_INPUT_ENABLE(p_``pre``_``suffix``_i[index], pre``_``suffix``_i_int[index]));
 
-// **************************************************************************************************************
-// PDT12DG foo_0_o (.PAD(p_foo_o[0]), .OEN(foo_oen_int[0]), .I(foo_o_int[0]))
+`define BSG_IO_IN_V(name,index,padtype) \
+    padtype name``_``index``_i (`BIDIR_PAD_INPUT_ENABLE(p_``name``_i[index], name``_i_int[index]));
 
-`define BSG_IO_OUT_V(name,index,padtype)                                                                                           \
-                                                                                                                                   \
-     padtype name``_``index``_o (.PAD(p_``name``_o[index])  `BSG_OUTPUT_ENABLE(name``_oen_int[``index]), .A(name``_o_int[``index]));
+// *****************************************************************************
+//
+// Begin IO Input Definitions
+//
 
+`define BSG_IO_OUT(name,padtype) \
+    wire name``_o_int;           \
+    padtype name``_o (`BIDIR_PAD_OUTPUT_ENABLED(p_``name``_o, name``_o_int));
 
+`define BSG_IO_OUT_V(name,index,padtype) \
+    padtype name``_``index``_o (`BIDIR_PAD_OUTPUT_ENABLED(p_``name``_o[index], name``_o_int[``index]));
 
-// **************************************************************************************************************
-// PDT12DGZ sdo_A_sclk_ex_o (.PAD(p_sdo_sclk_ex_o[0]), .OEN(sdo_sclk_ex_oen_int[0], .I(sdo_sclk_ex_o_int[0]))); --> BSG_OUT_AO(sdo,sclk_ex,A,0)
+`define BSG_IO_OUT_A(pre,suffix,letter,num,padtype) \
+    padtype pre``_``letter``_``suffix``_o (`BIDIR_PAD_OUTPUT_ENABLED(p_``pre``_``suffix``_o[num], pre``_``suffix``_o_int[num]));
 
-`define BSG_IO_OUT_A(pre,suffix,letter,num,padtype)                                                                                                            \
-                                                                                                                                                               \
-                                                                                                                                                               \
-  padtype pre``_``letter``_``suffix``_o (.PAD(p_``pre``_``suffix``_o[num])  `BSG_OUTPUT_ENABLE(pre``_``suffix``_oen_int[num]), .A(pre``_``suffix``_o_int[num]));
-
-
-// Physical only cells
+// *****************************************************************************
+//
+// Begin Physical Only Definitions
+//
 `define BSG_PHYSICAL_ONLY_PAD(name,padtype) \
-  padtype name ();
+    padtype name ();
 
-// shared macros
+
+// *****************************************************************************
+//
+// Source Shared Macros
+//
 `include "bsg_iopad_meta_macros.v"
 
 //
+// END TOP LEVEL I/O MACROS (16 nm)
 //
-// END TOP LEVEL I/O MACROS (180 nm)
-// **********************************************************************
+// *****************************************************************************
