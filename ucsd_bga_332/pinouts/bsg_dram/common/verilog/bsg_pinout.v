c2159b5db177dd9335c8940731ed2417c62a09dd
Shaolin_Xie <shx089@eng.ucsd.edu>
2018-05-05 11:19:53 -0700

    [40nm-tapeout] refactore the pinout.v with verilog vector expression

--- ucsd_bga_332/pinouts/bsg_dram/common/verilog/bsg_pinout.v
+++ ucsd_bga_332/pinouts/bsg_dram/common/verilog/bsg_pinout.v
@@ -2,51 +2,24 @@
  // *************************************************************************
  // DDR DRAM interface signals
  //
+ localparam dram_data_width_lp = 16;
+ localparam dram_addr_width_lp = 14;
+ localparam dram_strobe_width_lp = dram_data_width_lp >> 3;
+ localparam dram_mask_width_lp   = dram_data_width_lp >> 3;
  // Data mask signals
-   output p_ddr_dm_0_o
- , output p_ddr_dm_1_o
+   output[dram_strobe_width_lp-1:0] p_ddr_dm_o
 
  // DQ strobe signals, single-ended for DDR and differential for DDR2/3
- , inout  p_ddr_dqs_0_p_io
- , inout  p_ddr_dqs_1_p_io
+ , inout [dram_strobe_width_lp-1:0] p_ddr_dqs_p_io
 
  // DQ signals
- , inout  p_ddr_dq_0_io
- , inout  p_ddr_dq_1_io
- , inout  p_ddr_dq_2_io
- , inout  p_ddr_dq_3_io
- , inout  p_ddr_dq_4_io
- , inout  p_ddr_dq_5_io
- , inout  p_ddr_dq_6_io
- , inout  p_ddr_dq_7_io
- , inout  p_ddr_dq_8_io
- , inout  p_ddr_dq_9_io
- , inout  p_ddr_dq_10_io
- , inout  p_ddr_dq_11_io
- , inout  p_ddr_dq_12_io
- , inout  p_ddr_dq_13_io
- , inout  p_ddr_dq_14_io
- , inout  p_ddr_dq_15_io
+ , inout [dram_data_width_lp-1:0]   p_ddr_dq_io
 
  // Row/Column address signals, 16 bits at maximum
- , output p_ddr_a_0_o
- , output p_ddr_a_1_o
- , output p_ddr_a_2_o
- , output p_ddr_a_3_o
- , output p_ddr_a_4_o
- , output p_ddr_a_5_o
- , output p_ddr_a_6_o
- , output p_ddr_a_7_o
- , output p_ddr_a_8_o
- , output p_ddr_a_9_o
- , output p_ddr_a_10_o
- , output p_ddr_a_11_o
- , output p_ddr_a_12_o
- , output p_ddr_a_13_o
+ , output[dram_addr_width_lp-1:0]   p_ddr_a_0_o
 
  // Bank address signals, 3 bits at maximum
- , output p_ddr_ba_0_o
- , output p_ddr_ba_1_o
+ , output[dram_mask_width_lp-1:0]   p_ddr_ba_o
 
  // Control signals, p_ddr_odt_o and p_ddr_reset_o can be added to be
  // compatible with DDR2/3
@@ -60,26 +33,18 @@
  , output p_ddr_ck_p_o
  , output p_ddr_ck_n_o
 
-
-
  // *************************************************************************
  // Comm Link signals
  //
  // input clock signal
+ localparam comm_link_data_width_lp = 8;
  , input  p_sdi_sclk_i
 
  // input valid signal
  , input  p_sdi_ncmd_i
 
  // input data signals
- , input  p_sdi_data_0_i
- , input  p_sdi_data_1_i
- , input  p_sdi_data_2_i
- , input  p_sdi_data_3_i
- , input  p_sdi_data_4_i
- , input  p_sdi_data_5_i
- , input  p_sdi_data_6_i
- , input  p_sdi_data_7_i
+ , input  [comm_link_data_width_lp-1:0] p_sdi_data_i
 
  // output token signal
  , output p_sdi_token_o
@@ -91,14 +56,7 @@
  , output p_sdo_ncmd_o
 
  // output data signals
- , output p_sdo_data_0_o
- , output p_sdo_data_1_o
- , output p_sdo_data_2_o
- , output p_sdo_data_3_o
- , output p_sdo_data_4_o
- , output p_sdo_data_5_o
- , output p_sdo_data_6_o
- , output p_sdo_data_7_o
+ , output [comm_link_data_width_lp-1:0] p_sdo_data_0_o
 
  // input token signal
  , input  p_sdo_token_i
@@ -128,11 +86,9 @@
  , input  p_misc_T_1_i
 
 
-
  // *************************************************************************
  // Global input signals
  , input  p_reset_i
-
 
 
  // *************************************************************************
