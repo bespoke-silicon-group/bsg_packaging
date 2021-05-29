de75f350d193b6e29c26420ca712c2c4f2b936a7
Scott Davidson <scottdavidson1992@gmail.com>
2021-05-29 15:27:29 -0700

    Read RDL cache if available, otherwise build the RDL

--- basejump_fcbga_785/pinouts/bsg_bigblade_v0/gf_14_invecas_1p8v/tcl/post_io_placement.tcl
+++ basejump_fcbga_785/pinouts/bsg_bigblade_v0/gf_14_invecas_1p8v/tcl/post_io_placement.tcl
@@ -11,41 +11,53 @@ puts "BSG-info: Running script [info script]\n"
 
 ### ROUTE RDL ##################################################################
 
-set io_guide_map [bsg_rdl_create_io_guide_map "$::env(BSG_PACKAGING_PINOUT_SPEC_DIR)/bsg_padring_bump_map.csv"]
+set olddir [pwd]
+cd $::env(BSG_PACKAGING_DIR)
+set bsg_packaging_commit_hash [exec git rev-parse HEAD]
+cd $olddir
 
-bsg_rdl_create_blockages
-bsg_rdl_route_io_supply
+if {[file exists $PDK_ROOT_DIR/bsg/rdl/basejump_fcbga_785_rdl.$bsg_packaging_commit_hash.def]} {
+  puts "BSG-info: reading RDL def file $PDK_ROOT_DIR/bsg/rdl/basejump_fcbga_785_rdl.$bsg_packaging_commit_hash.def"
+  read_def $PDK_ROOT_DIR/bsg/rdl/basejump_fcbga_785_rdl.$bsg_packaging_commit_hash.def
+} else {
+  puts "BSG-info: building RDL from source (file not found: $PDK_ROOT_DIR/bsg/rdl/basejump_fcbga_785_rdl.$bsg_packaging_commit_hash.def)"
 
-bsg_rdl_route_guide $io_guide_map DL5
-bsg_rdl_route_guide $io_guide_map DL4
-bsg_rdl_route_guide $io_guide_map DL3
-bsg_rdl_route_guide $io_guide_map DL2
-bsg_rdl_route_guide $io_guide_map DL1
+  set io_guide_map [bsg_rdl_create_io_guide_map "$::env(BSG_PACKAGING_PINOUT_SPEC_DIR)/bsg_padring_bump_map.csv"]
 
-bsg_rdl_route_guide $io_guide_map DR5
-bsg_rdl_route_guide $io_guide_map DR4
-bsg_rdl_route_guide $io_guide_map DR3
-bsg_rdl_route_guide $io_guide_map DR2
-bsg_rdl_route_guide $io_guide_map DR1
+  bsg_rdl_create_blockages
+  bsg_rdl_route_io_supply
 
-bsg_rdl_route_guide $io_guide_map ML0
-bsg_rdl_route_guide $io_guide_map DL0
-bsg_rdl_route_guide $io_guide_map IT0
-bsg_rdl_route_guide $io_guide_map CT0
-bsg_rdl_route_guide $io_guide_map IT1
-bsg_rdl_route_guide $io_guide_map DR0
-bsg_rdl_route_guide $io_guide_map MR0
+  bsg_rdl_route_guide $io_guide_map DL5
+  bsg_rdl_route_guide $io_guide_map DL4
+  bsg_rdl_route_guide $io_guide_map DL3
+  bsg_rdl_route_guide $io_guide_map DL2
+  bsg_rdl_route_guide $io_guide_map DL1
 
-bsg_rdl_route_guide $io_guide_map ML1
-bsg_rdl_route_guide $io_guide_map DL6
-bsg_rdl_route_guide $io_guide_map DL7
-bsg_rdl_route_guide $io_guide_map CB0
-bsg_rdl_route_guide $io_guide_map DR7
-bsg_rdl_route_guide $io_guide_map DR6
-bsg_rdl_route_guide $io_guide_map MR1
+  bsg_rdl_route_guide $io_guide_map DR5
+  bsg_rdl_route_guide $io_guide_map DR4
+  bsg_rdl_route_guide $io_guide_map DR3
+  bsg_rdl_route_guide $io_guide_map DR2
+  bsg_rdl_route_guide $io_guide_map DR1
 
-optimize_rdl_routes -layer LB
-bsg_rdl_remove_blockages
+  bsg_rdl_route_guide $io_guide_map ML0
+  bsg_rdl_route_guide $io_guide_map DL0
+  bsg_rdl_route_guide $io_guide_map IT0
+  bsg_rdl_route_guide $io_guide_map CT0
+  bsg_rdl_route_guide $io_guide_map IT1
+  bsg_rdl_route_guide $io_guide_map DR0
+  bsg_rdl_route_guide $io_guide_map MR0
+
+  bsg_rdl_route_guide $io_guide_map ML1
+  bsg_rdl_route_guide $io_guide_map DL6
+  bsg_rdl_route_guide $io_guide_map DL7
+  bsg_rdl_route_guide $io_guide_map CB0
+  bsg_rdl_route_guide $io_guide_map DR7
+  bsg_rdl_route_guide $io_guide_map DR6
+  bsg_rdl_route_guide $io_guide_map MR1
+
+  optimize_rdl_routes -layer LB
+  bsg_rdl_remove_blockages
+}
 
 puts "BSG-info: Completed script [info script]\n"
 
